852cec504c37add3e29bb863a7d4dc7dde00bc52
ketan Muley <k25muley@gmail.com>
2020-10-26 17:28:44 +0530

    commit 7

--- modules/db_parameter_group/versions.tf
+++ modules/db_parameter_group/versions.tf
@@ -1,11 +1,12 @@
 terraform {
   required_version = ">= 0.12.6"
 
-  #required_providers {
+  required_providers {
+    aws = ">= 2.49"
+  }
+}
+
+ #required_providers {
    # aws = ">= 2.49"
   #}
 
-  provider "aws" {
-    version = "~> 2.17"
-    region = "us-east-1"
-}
--- modules/db_subnet_group/versions.tf
+++ modules/db_subnet_group/versions.tf
@@ -1,12 +1,8 @@
 terraform {
   required_version = ">= 0.12.6"
 
-  #required_providers {
-    #aws = ">= 2.49"
-  #}
-
-  provider "aws" {
-    version = "~> 2.17"
-    region = "us-west-2"
+  required_providers {
+    aws = ">= 2.49"
+  }
 }
 
