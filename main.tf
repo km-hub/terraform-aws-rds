c41bf74f8886aa3af647d7b7be10a70b86d5e2e2
ketan Muley <k25muley@gmail.com>
2020-10-26 17:09:59 +0530

    commit 2

--- main.tf
+++ main.tf
@@ -67,7 +67,7 @@ module "db_instance" {
   license_model     = var.license_model
 
   name                                = var.name
-  #region                              = var.region
+  region                              = var.region
   username                            = var.username
   password                            = var.password
   port                                = var.port
