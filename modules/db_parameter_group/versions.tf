f460e5bef14e40cc76236d38b9abdd6abcdbed5e
ketan Muley <k25muley@gmail.com>
2020-10-26 17:20:27 +0530

    commit 4

--- modules/db_parameter_group/versions.tf
+++ modules/db_parameter_group/versions.tf
@@ -1,7 +1,11 @@
 terraform {
   required_version = ">= 0.12.6"
 
-  required_providers {
-    aws = ">= 2.49"
-  }
+  #required_providers {
+   # aws = ">= 2.49"
+  #}
+
+  provider "aws" {
+    version = "~> 2.17"
+    region = "us-east-1"
 }
